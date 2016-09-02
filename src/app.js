656c3b2f5edfbbf42b9fdf574154a2c2d87dc7da
mcellmi <mike.mcelligott@tecnotree.com>
2016-09-03 00:27:43 +0100

    Added web server

--- src/app.js
+++ src/app.js
@@ -7,7 +7,8 @@
 // Add configuration management
 const   fs      = require('fs'),
         path    = require('path'),
-        nconf   = require('nconf');
+        nconf   = require('nconf'),
+        _       = require('lodash');
 
 // Create a main logging instance which can be used to co-ordinate logging
 // across all modules
@@ -33,6 +34,17 @@ const   log             = bunyan.createLogger({
 const Graph = require('../src/graph');
 const Search = require('../src/graphSearch');
 
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(express.static('public'));
+
+
+
 let graph = new Graph({
         log: log,
         dataFile: nconf.get('data:file'),
@@ -45,7 +57,61 @@ graph.readData()
 
         let search = new Search(graph,{log: log});
 
-        search.getPaths({source:"DOUG_BARTOLOMEO", destination:"*"});
+        app.get('/index.html', function (req, res) {
+            res.sendFile( __dirname + "/" + "index.html" );
+        })
+        app.get('/bundle.js', function (req, res) {
+            res.sendFile( __dirname + "/" + "bundle.js" );
+        })
+
+        app.get('/search', function (req, res) {
+
+            // Prepare output in JSON format
+            let searchConditions = {
+                source:req.query.source,
+                destination:req.query.destination,
+                searchType:"shortestPath"
+            };
+            let results = search.getPaths(searchConditions);
+            let response = "";
+            for (let x = 0; x < results.length; ++x) {
+                response = _.join(results[x], "->") + "<br/>";
+            }
+            console.log(response);
+            res.end(JSON.stringify(results));
+        })
+
+        app.get('/searchAll', function (req, res) {
+            let results = search.revealContacts(req.query.source);
+            let response = "";
+            for (let x = 0; x < results.length; ++x) {
+                response = _.join(results[x], "->") + "<br/>";
+            }
+            console.log(response);
+            res.end(JSON.stringify(results));
+        })
+
+        var server = app.listen(8081, function () {
+
+            var host = server.address().address
+            var port = server.address().port
+
+            console.log("Example app listening at http://%s:%s", host, port)
+
+        })
+
+
+        //
+        //
+        // search.getPaths({source:"DOUG_BARTOLOMEO", destination:"*"});
+        //
+        // search.revealContacts("DOUG_BARTOLOMEO");
+        //
+        // let results = search.getPaths({source:"DOUG_BARTOLOMEO", destination:"JOSPEH_MONTUORI", searchType:"shortestPath"});
+        //
+        // for (let x = 0; x < results.length; ++x) {
+        //     console.log(_.join(results[x],"->"));
+        // }
     })
     .catch((error) => {
         log.error(error);
